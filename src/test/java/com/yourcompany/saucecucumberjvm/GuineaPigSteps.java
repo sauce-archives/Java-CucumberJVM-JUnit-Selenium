3d48018bc94e70f6f3e859bb021c7599ff462ac4
ndmanvar <ndmanvar@yahoo.com>
2016-10-11 12:58:19 -0700

    using https

--- src/test/java/com/yourcompany/saucecucumberjvm/GuineaPigSteps.java
+++ src/test/java/com/yourcompany/saucecucumberjvm/GuineaPigSteps.java
@@ -33,22 +33,22 @@ import java.util.concurrent.TimeUnit;
 import java.net.URL;
 
 public class GuineaPigSteps {
-	
+
 	public static final String USERNAME = System.getenv("SAUCE_USERNAME");
 	public static final String ACCESS_KEY = System.getenv("SAUCE_ACCESS_KEY");
-	public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+	public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
 	public static WebDriver driver;
 	public String sessionId;
 	public boolean testResults;
 	public String jobName;
-	
+
 	public void UpdateResults(boolean testResults) throws JSONException, ClientProtocolException, IOException {
 		SauceREST saucerest = new SauceREST(USERNAME, ACCESS_KEY);
     	Map<String, Object> updates = new HashMap<String, Object>();
         updates.put("passed", testResults);
         saucerest.updateJobInfo(sessionId, updates);
 	}
-	
+
 	@Before
 	public void setUp(Scenario scenario) throws Throwable {
         DesiredCapabilities caps = new DesiredCapabilities();
@@ -59,30 +59,30 @@ public class GuineaPigSteps {
 
 	    driver = new RemoteWebDriver(new URL(URL), caps);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        
+
         jobName = caps.getCapability("name").toString();
-        
+
         sessionId = (((RemoteWebDriver) driver).getSessionId()).toString();
         testResults = false;
 	}
-	
+
 	@Given("^I am on the Sauce Labs Guinea Pig test page$")
 	public void user_is_on_guinea_pig_page() throws Throwable {
         driver.get("https://saucelabs.com/test/guinea-pig");
 	}
- 
+
 	@When("^I click on the link$")
 	public void user_click_on_the_link() throws Throwable {
 		driver.findElement(By.linkText("i am a link")).click();
 	}
- 
+
 	@Then("^I should see a new page$")
 	public void new_page_displayed() throws Throwable {
 		String page_title = driver.getTitle();
 		Assert.assertTrue(page_title.equals("I am another page title - Sauce Labs"));
 		testResults = true;
 	}
-	
+
 	@After
 	public void tearDown() throws Throwable {
 		driver.quit();
